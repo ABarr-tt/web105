9e4764ef9f210bfd26ae3ccc0c45cb0107d66b66
Aidan Barrett <142757349+ABarr-tt@users.noreply.github.com>
2023-11-05 15:32:16 -0600

    Add files via upload

---
+++ grid-layout/change.js
@@ -0,0 +1,19 @@
+function change01() {
+    document.getElementById("large").src = "images/card-6.jpg";
+}
+
+function change02() {
+    document.getElementById("large").src = "images/dog_hat.jpg";
+}
+
+function change03() {
+    document.getElementById("large").src = "images/mom_and_child.jpg";
+}
+
+function change04() {
+    document.getElementById("large").src = "images/woman_donuts.jpg";
+}
+
+function change05() {
+    document.getElementById("large").src = "images/woman_with_food.jpg";
+}
